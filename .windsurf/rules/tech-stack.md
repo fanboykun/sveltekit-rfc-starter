ad49a446b444ac334a7a85ce20c7801962927b8a
fanboykun <irfanramadhan1812@gmail.com>
2025-08-14 07:46:48 +0700

    add llm docs, editor settings

---
+++ .windsurf/rules/tech-stack.md
@@ -0,0 +1,9 @@
+---
+trigger: always_on
+---
+
+---
+
+## trigger: always_on
+
+this project uses svelte version 5, sveltekit version 2, newsest sveltekit feature called remote functions tailwindcss version 4, shadcn-svelte, drizzle with postgres sql, redis with ioredis. Make sure the code suggestion is updated to the current version of the framework and library used.
--- eslint.config.js
+++ eslint.config.js
@@ -23,11 +23,23 @@ export default ts.config(
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			]
 		}
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			],
+			'svelte/require-each-key': 'off',
+			'svelte/no-inspect': 'off'
+		},
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
